4befffc2add44260c053398a8d5a0ba3b75c42c3
Makka <makkanev@gmail.com>
2024-08-15 17:27:43 +0300

    add Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,22 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        Cat been3 = (Cat) applicationContext.getBean("Cat" );
+        Cat been4 = (Cat) applicationContext.getBean("Cat" );
+
+        if (bean==bean2){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+        if (been3==been4) {
+            System.out.println(true);
+        }else {
+            System.out.println(false);;
+        }
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+
 @Configuration
 public class AppConfig {
  
@@ -10,5 +11,11 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name = "Cat")
+    @Scope( "prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        return cat;
     }
 }
